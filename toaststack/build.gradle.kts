70243cc1ab283139797c2543bc6e45bf1134295b
Ghattamaneni Venkateswararao <zeevy@mb4p.local>
2026-03-23 18:03:14 +0530

    chore: bump version to 1.0.1

--- toaststack/build.gradle.kts
+++ toaststack/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
 
                 groupId = "com.siliconcircuits"
                 artifactId = "toaststack"
-                version = "1.0.0"
+                version = "1.0.1"
 
                 pom {
                     name.set("ToastStack")
